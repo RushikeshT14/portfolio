a3e82f4166592db41412df114d276983afa98ac6
RushikeshT14 <rushitodkari0108@gmail.com>
2026-05-20 12:42:51 +0530

    resume updation

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -50,9 +50,7 @@ function Projects() {
                 robust backend API, ensuring fast, reliable trade execution and
                 data handling.Includes portfolio tracking, watchlists, and
                 responsive dashboard components.Optimized for desktop usage,
-                with mobile view still under development.Designed as an
-                end-to-end full-stack project showcasing UI/UX, backend logic,
-                and real-time integrations.
+                with mobile view still under development.
               </p>
               <div>
                 <i class="fa-brands fa-react"></i>
@@ -99,7 +97,7 @@ function Projects() {
           >
             <div className="ProjectCard">
               <a href="https://airbnb-clone-da6l.onrender.com" target="_blank">
-                StayEase 🏡 &#8592;{" "}
+                StayEase &#8592;{" "}
               </a>
               <p>
                 WanderLust is a full-stack web application inspired by Airbnb.
@@ -158,26 +156,15 @@ function Projects() {
             data-aos-easing="ease-in-sine"
           >
             <div className="ProjectCard">
-              <a href="https://airbnb-clone-da6l.onrender.com" target="_blank">
-                Coming soon &#8592;{" "}
+              <a href="https://www.rhythmandtunes.in/" target="_blank">
+              Rhythm & Tunes &#8592;{" "}
               </a>
-              <p>
-                WanderLust is a full-stack web application inspired by Airbnb.
-                It features a secure authentication system with login, signup,
-                and session handling using Passport.js. Users can leave reviews
-                and ratings, making the platform interactive and
-                community-driven. The design is fully responsive for desktop and
-                mobile, styled with Bootstrap and custom CSS. Deployed on Render
-                with MongoDB Atlas, WanderLust showcases the integration of
-                backend logic, frontend design, and cloud deployment.
+              <p>A modern and responsive website for a professional music academy. The platform showcases various music courses, instruments, and training programs in a clean and user-friendly way. I focused on creating an engaging UI/UX with smooth navigation, responsive layouts, and optimized performance across all devices. I also worked on improving accessibility, loading speed, and overall user experience to make the site both visually appealing and functional. 
               </p>
               <div>
                 <i class="fa-brands fa-html5"></i>
                 <i class="fa-brands fa-css"></i>
-                <i class="fa-brands fa-bootstrap"></i>
                 <i class="fa-brands fa-square-js"></i>
-                <i class="fa-brands fa-node-js"></i>
-                <i class="fa-solid fa-database"></i>
               </div>
             </div>
           </div>
